b6287eb756537b675de3a763abbd0b30add80666
Fitano Pranata <fitanopranata@gmail.com>
2023-05-01 20:42:10 +0700

    changing dashboard ui using bootstrap

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -50,7 +50,7 @@
                 Welcome back, {{ auth()->user()->name }}
               </a>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Dashboard</a></li>
+                <li><a class="dropdown-item" href="/dashboard">Dashboard</a></li>
                 <li><hr class="dropdown-divider"></li>
                 <li>
                   <form action="/logout" method="post">
